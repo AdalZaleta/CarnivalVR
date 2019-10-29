63afaae319f2404222154f09ba10f8145d44c598
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2019-10-29 09:44:24 -0600

    Initial commit

---
+++ Assets/_Game/Controler Script/Move_Car.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Car : MonoBehaviour
+{
+    public enum typeButtom
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+    public typeButtom myType;
+    public Grid mapa;
+    public GameObject character;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Me estan dando click " + gameObject.name);
+        if(myType == typeButtom.Up)
+        {
+            character.transform.localPosition = mapa.CellToLocal(mapa.WorldToCell(character.transform.position) + new Vector3Int(0, 0, 1));
+        }
+        if (myType == typeButtom.Right)
+        {
+            character.transform.localPosition = mapa.CellToLocal(mapa.WorldToCell(character.transform.position) + new Vector3Int(1, 0, 0));
+        }
+        if (myType == typeButtom.Down)
+        {
+            character.transform.localPosition = mapa.CellToLocal(mapa.WorldToCell(character.transform.position) + new Vector3Int(0, 0, -1));
+        }
+        if (myType == typeButtom.Left)
+        {
+            character.transform.localPosition = mapa.CellToLocal(mapa.WorldToCell(character.transform.position) + new Vector3Int(-1, 0, 0));
+        }
+    }
+}
---
+++ Assets/_Game/Paredes_Laberinto.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paredes_Laberinto : MonoBehaviour
+{
+
+    public enum typePared
+    {
+        PARED = 1,
+        PARED_HOYO = 2,
+        SIN_PARED = 0
+    }
+
+    public typePared myType;
+    public GameObject[] paredes;
+
+    [ContextMenu("Update The Piece")]
+    public void UpdtaePiece()
+    {
+        foreach (GameObject g in paredes)
+        {
+            g.SetActive(false);
+        }
+        paredes[(int)myType].SetActive(true);
+    }
+
+    public void OnValidate()
+    {
+        UpdtaePiece();
+    }
+}
